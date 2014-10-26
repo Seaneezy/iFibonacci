b55aacf477fc9a66e70ca1570ea47c1abd612c3d
Sean Sponsler <seaneezer@yahoo.com>
2014-10-26 14:23:20 -0700

    fib1 with comments

--- fib.py
+++ fib.py
@@ -1,9 +1,14 @@
 def fib1(n):
     fibList=[1]
+    #starting values for a and b
     a, b=1, 1
+    #while the 2nd number is less than or equal to requested value
     while b<=n:
+        #append the 2nd number
         fibList.append(b)
+        #shift a and b up the sequence
         a,b=b,a+b
+        #return list of numbers in sequence
     return fibList
 """
 x=int(input("max num: "))
