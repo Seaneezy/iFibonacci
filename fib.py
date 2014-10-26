355c83147ed224e73131b1c9c6ae6210f3e3e049
Sean Sponsler <seaneezer@yahoo.com>
2014-10-26 14:11:23 -0700

    fib1 function
    w/o print

--- fib.py
+++ fib.py
@@ -1,9 +1,11 @@
-def fib2(n):
+def fib1(n):
     fibList=[1]
     a, b=1, 1
     while b<=n:
         fibList.append(b)
         a,b=b,a+b
     return fibList
-req=int(input("max num: "))
-print(fib2(req))
+"""
+x=int(input("max num: "))
+print(fib2(x))
+"""
